5fca3b6c55c530ff238241429ece2bebfdaadf3e
Sergey <s3rgencio@gmail.com>
2023-02-19 18:21:52 +0300

    Added field in Gin server config for CORS. Added Role field to SignUp struct.

--- internal/transport/http/config.go
+++ internal/transport/http/config.go
@@ -1,11 +1,25 @@
 package http
 
+import (
+	"github.com/gin-contrib/cors"
+	"time"
+)
+
 type Config struct {
 	Port string
+	Cors cors.Config
 }
 
 func NewConfig() *Config {
 	return &Config{
 		Port: "8080",
+		Cors: cors.Config{
+			AllowOrigins:     []string{"http://localhost:3000"},
+			AllowMethods:     []string{"GET", "POST"},
+			AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type"},
+			ExposeHeaders:    []string{"Content-Length"},
+			AllowCredentials: true,
+			MaxAge:           12 * time.Hour,
+		},
 	}
 }
